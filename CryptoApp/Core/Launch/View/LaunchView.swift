1fc1af6a0251d7ab5f1ab4312fec9d24b8d1ea48
Roman Romanov <rromanov.vk@gmail.com>
2026-04-07 13:28:20 +0500

    adding custom Launch Screen with animation

---
+++ CryptoApp/Core/Launch/View/LaunchView.swift
@@ -0,0 +1,68 @@
+//
+//  LaunchView.swift
+//  CryptoApp
+//
+//  Created by Roman Romanov on 07.04.2026.
+//
+
+import Combine
+import SwiftUI
+
+struct LaunchView: View {
+
+	@Binding var showLaunchView: Bool
+
+	@State private var loadingText: [String] = "Loading your portfolio...".map { String($0) }
+	@State private var showLoadingText: Bool = false
+
+	@State private var counter: Int = 0
+	@State private var loops: Int = 0
+
+	private let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+
+    var body: some View {
+		ZStack {
+			Color.launch.background
+				.ignoresSafeArea()
+
+			Image(.Images.logoTransparent)
+				.resizable()
+				.frame(width: 100, height: 100)
+
+			ZStack {
+				if showLoadingText {
+					HStack(spacing: 0) {
+						ForEach(loadingText.indices, id: \.self) { index in
+							Text(loadingText[index])
+								.font(.headline)
+								.fontWeight(.heavy)
+								.foregroundStyle(Color.launch.accent)
+								.offset(y: counter == index ? -5 : 0)
+						}
+					}
+					.transition(.scale.animation(.easeIn))
+				}
+			}
+			.offset(y: 70)
+		}
+		.onAppear {
+			showLoadingText.toggle()
+		}
+		.onReceive(timer) { _ in
+			let lastIndex = loadingText.count - 1
+			if lastIndex == counter {
+				loops += 1
+				if loops >= 2 {
+					showLaunchView = false
+				}
+			}
+			withAnimation(.easeIn) {
+				counter = lastIndex == counter ? 0 : counter + 1
+			}
+		}
+    }
+}
+
+#Preview {
+	LaunchView(showLaunchView: .constant(true))
+}
